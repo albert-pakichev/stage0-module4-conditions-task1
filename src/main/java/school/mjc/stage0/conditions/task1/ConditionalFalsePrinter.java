2583bc559cfa5eebe6a7af86ae86970bc63db07e
albert-pakichev <69592416+albert-pakichev@users.noreply.github.com>
2023-06-25 22:19:20 +0500

    Update ConditionalFalsePrinter.java

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        int a = -1;
+        int b = 0;
+        if(a>b) {
+        System.out.println(true);
+        }
+        else {
+        System.out.println(false);    
+        }
     }
 }
